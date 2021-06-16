7bb192e8ab2c3f179d481e3490f923cdf94314bc
Vijay Jammi <vjammi@gmail.com>
2021-06-15 23:16:21 -0400

    PriorityQueue usage

--- src/main/java/ds/binaryheap/PriorityQueueJavaUtil.java
+++ src/main/java/ds/binaryheap/PriorityQueueJavaUtil.java
@@ -3,14 +3,13 @@ package ds.binaryheap;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.PriorityQueue;
-import java.util.Random;
 
 public class PriorityQueueJavaUtil {
 
     class MaxIntegerComparator implements Comparator<Integer>{
         @Override
         public int compare(Integer x, Integer y){
-            return x - y;
+            return y - x;
         }
     }
 
@@ -25,15 +24,15 @@ public class PriorityQueueJavaUtil {
     private PriorityQueue<Integer> minHeap;
     private PriorityQueue<Integer> maxHeap;
 
-    public PriorityQueueJavaUtil(int size) {
+    public PriorityQueueJavaUtil(int[] nums) {
         //this.minHeap = new PriorityQueue<>(size); // Default - MinIntegerComparator
-        this.minHeap = new PriorityQueue<>(size, new MinIntegerComparator()); // Default - MinIntegerComparator
-        this.maxHeap = new PriorityQueue<>(size, new MaxIntegerComparator()); // MaxIntegerComparator
+        this.minHeap = new PriorityQueue<>(nums.length+1, new MinIntegerComparator()); // Default - MinIntegerComparator
+        this.maxHeap = new PriorityQueue<>(nums.length+1, new MaxIntegerComparator()); // MaxIntegerComparator
 
-        for(int i = 1; i<= size; ++i){
-            int data = new Random().nextInt(100) +1; //number between 0 to 100
-            minHeap.add(data);
-            maxHeap.add(data);
+        for(int i = 1; i< nums.length; i++){
+            //int data = new Random().nextInt(100) +1; //number between 0 to 100
+            minHeap.add(nums[i]);
+            maxHeap.add(nums[i]);
         }
     }
 
@@ -52,12 +51,15 @@ public class PriorityQueueJavaUtil {
     }
 
     public static void main(String[] args) {
-        PriorityQueueJavaUtil pq = new PriorityQueueJavaUtil(10);
+        int[] nums = new int[]{15,14,13,20,15,10,9,8,7,4,3};
+        PriorityQueueJavaUtil pq = new PriorityQueueJavaUtil(nums);
 
-        System.out.print("Min Heap");
+        System.out.println("Min Heap");
         pq.iterateMinHeap();
 
-        System.out.print("Max Heap");
+        System.out.println("");
+
+        System.out.println("Max Heap");
         pq.iterateMaxHeap();
     }
 }
