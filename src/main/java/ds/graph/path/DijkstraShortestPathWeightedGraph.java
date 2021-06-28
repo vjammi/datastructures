88fb5e3f5f06e2cf3da1ec19c20b411db4f77c97
Vijay Jammi <vjammi@gmail.com>
2021-06-28 01:11:57 -0400

    Dijkstra's ShortestPath for Weighted Graph

--- src/main/java/ds/graph/path/DijkstraShortestPathWeightedGraph.java
+++ src/main/java/ds/graph/path/DijkstraShortestPathWeightedGraph.java
@@ -4,32 +4,37 @@ import javafx.util.Pair;
 import java.util.*;
 
 public class DijkstraShortestPathWeightedGraph {
-    private List<Pair<Integer,Integer>>[] adjList;
+    private Map<Integer, List<Pair<Integer,Integer>> > adjList;
 
     public DijkstraShortestPathWeightedGraph(int noOfVertices) {
-        adjList=new ArrayList[noOfVertices];
+        adjList =new HashMap<>();
+
+        for (int i=0; i<noOfVertices; i++){
+            adjList.put(i, new ArrayList<Pair<Integer,Integer>>());
+        }
 
         //Pair(vertex, weight)
-        List<Pair<Integer,Integer>> list=new ArrayList<>();
-        list.add(new Pair(1,4));
-        list.add(new Pair(2,1));
-        adjList[0]=list;
+        //List<Pair<Integer,Integer>> list=new ArrayList<>();
+        adjList.get(0).add(new Pair(1,4));
+        adjList.get(0).add(new Pair(2,1));
+        //adjList[0]=list;
 
-        list=new ArrayList<>();
-        list.add(new Pair(4,4));
-        adjList[1]=list;
+        //list=new ArrayList<>();
+        adjList.get(1).add(new Pair(4,4));
+        //adjList[1]=list;
 
-        list=new ArrayList<>();
-        list.add(new Pair(1,2));
-        list.add(new Pair(3,4));
-        adjList[2]=list;
+        //list=new ArrayList<>();
+        adjList.get(2).add(new Pair(1,2));
+        adjList.get(2).add(new Pair(3,4));
+        //adjList[2]=list;
 
-        list=new ArrayList<>();
-        list.add(new Pair(4,4));
-        adjList[3]=list;
+        //list=new ArrayList<>();
+        adjList.get(3).add(new Pair(4,4));
+        //adjList[3]=list;
 
-        list=new ArrayList<>();
-        adjList[4]=list;
+        //list=new ArrayList<>();
+        //adjList[4]=list;
+        //adjList.get(4)
     }
 
     private void dijkstra(int startVertex, int noOfVertices) {
@@ -44,20 +49,22 @@ public class DijkstraShortestPathWeightedGraph {
         priorityQueue.add(new Pair<>(startVertex,0));
 
         while(!priorityQueue.isEmpty()){
-            Pair<Integer,Integer> vertexPair = priorityQueue.remove();
-            Integer vertex = vertexPair.getKey();
-            List<Pair<Integer,Integer>> neighbors = adjList[vertex];
+            Pair<Integer,Integer> pair = priorityQueue.remove();
+            Integer vertex = pair.getKey();
 
+            List<Pair<Integer,Integer>> neighbors = adjList.get(vertex);
             for(Pair<Integer,Integer> neighbor: neighbors){
-                int neighboringVertex=neighbor.getKey();
-                int weight=neighbor.getValue();
-                int newDistance=distance[vertex] + weight;
+
+                int neighboringVertex = neighbor.getKey();
+                int weight            = neighbor.getValue();
+                int newDistance       = distance[vertex] + weight;
 
                 if(distance[neighboringVertex] == -1 || distance[neighboringVertex] > newDistance){
                     distance[neighboringVertex] = newDistance;
                     path[neighboringVertex] = vertex;
                     priorityQueue.add(new Pair<>(neighboringVertex,distance[neighboringVertex]));
                 }
+
             }
 
         }
