326fb7c94abe261cf00542000994565fde1288d2
Vijay Jammi <vjammi@gmail.com>
2021-07-14 16:38:44 -0400

    LC100 Same Tree

--- src/main/java/ds/patterns/trees/traversal/SameTree.java
+++ src/main/java/ds/patterns/trees/traversal/SameTree.java
@@ -1,4 +1,48 @@
 package ds.patterns.trees.traversal;
 
+    /**
+        // https://leetcode.com/problems/same-tree/
+        100. Same Tree
+        Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+        Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+        Input: p = [1,2,3], q = [1,2,3]
+        Output: true
+        Input: p = [1,2], q = [1,null,2]
+        Output: false
+        Input: p = [1,2,1], q = [1,1,2]
+        Output: false
+     */
 public class SameTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+
+        if (p != null && q!=null && p.val == q.val){
+            boolean left  = isSameTree(p.left, q.left);
+            boolean right = isSameTree(p.right, q.right);
+            if (left && right)
+                return true;
+            else
+                return false;
+        }else{
+            return false;
+        }
+    }
+
 }
