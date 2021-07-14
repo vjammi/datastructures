4eaabbfe424c122089924c26d78a2b27dafa34c7
Vijay Jammi <vjammi@gmail.com>
2021-07-14 16:57:36 -0400

    LC226 InvertBinaryTree

--- src/main/java/ds/patterns/trees/traversal/InvertBinaryTree.java
+++ src/main/java/ds/patterns/trees/traversal/InvertBinaryTree.java
@@ -4,8 +4,15 @@ package ds.patterns.trees.traversal;
     226. Invert Binary Tree
     Given the root of a binary tree, invert the tree, and return its root.
     Input: root = [4,2,7,1,3,6,9]
+                4
+            2      7
+          1  3   6   9
     Output: [4,7,2,9,6,3,1]
-    Input: root = [2,1,3]
+                4
+            7       2
+          9   6   3   1
+
+ Input: root = [2,1,3]
     Output: [2,3,1]
     Input: root = []
     Output: []
