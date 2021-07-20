23cf02c20335cc94a6d209873e90f0f281c2835f
Vijay Jammi <vjammi@gmail.com>
2021-07-20 00:23:47 -0400

    LC285 Inorder Successor in BST

---
+++ src/main/java/ds/binarysearchtree/InOrderSuccessor.java
@@ -0,0 +1,39 @@
+package ds.binarysearchtree;
+
+public class InOrderSuccessor {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        if (root == null)
+            return null;
+        successor(root, p);
+        return successor;
+    }
+
+    TreeNode successor  = null;
+    private void successor(TreeNode node, TreeNode p){
+        if (node == null)
+            return;
+
+        if (p.val < node.val) {
+            System.out.println(" < " +node.val +" "+ node.right);
+            successor = node;
+            successor(node.left, p);
+        }else if (p.val >= node.val) {
+            System.out.println(" > " +node.val +" "+ node.right);
+            successor(node.right, p);
+        }
+    }
+}
