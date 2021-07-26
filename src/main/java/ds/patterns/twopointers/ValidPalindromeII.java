c52e21e01d44badcdb6763fc6675c6b7aba5ce24
Vijay Jammi <vjammi@gmail.com>
2021-07-26 10:32:54 -0400

    2 pointers

---
+++ src/main/java/ds/patterns/twopointers/ValidPalindromeII.java
@@ -0,0 +1,15 @@
+package ds.patterns.twopointers;
+
+/**
+    680. Valid Palindrome II    https://leetcode.com/problems/valid-palindrome-ii/
+    Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+    Example 1:
+        Input: s = "aba"
+        Output: true
+    Example 2:
+        Input: s = "abca"
+        Output: true
+        Explanation: You could delete the character 'c'.
+ **/
+public class ValidPalindromeII {
+}
