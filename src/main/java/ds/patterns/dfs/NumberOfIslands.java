300cc7e0eadc1848a77810e4be312f51ae290ad0
Vijay Jammi <vjammi@gmail.com>
2021-07-26 18:29:37 -0400

    NumberOfIslands

--- src/main/java/ds/patterns/dfs/NumberOfIslands.java
+++ src/main/java/ds/patterns/dfs/NumberOfIslands.java
@@ -15,6 +15,11 @@ package ds.patterns.dfs;
 */
 public class NumberOfIslands {
 
+    /**
+     * Complexity Analysis
+     * Time complexity : O(M×N) where M is the number of rows and N is the number of columns.
+     * Space complexity : worst case O(M×N) in case that the grid map is filled with lands where DFS goes by M×N deep.
+     **/
     public int numIslands(char[][] grid) {
         int numOfIslands = 0;
         if (grid.length == 0)
