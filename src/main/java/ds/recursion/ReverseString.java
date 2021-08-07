8f47f79d173a10a8f007a376cb05b9344f681774
Vijay Jammi <vjammi@gmail.com>
2021-08-06 20:10:31 -0400

    Reverse a String

--- src/main/java/ds/recursion/ReverseString.java
+++ src/main/java/ds/recursion/ReverseString.java
@@ -24,6 +24,20 @@ public class ReverseString {
         exch(s, i, j);
         reverse2(s, i+1, j-1);
     }
+    public void reverseString(char[] s) {
+
+        System.out.println(s);
+
+        int i = 0;
+        int j = s.length-1;
+        while(i<=j){
+            exch(s, i, j);
+            i++;
+            j--;
+        }
+        System.out.println(s);
+    }
+
     private void exch(char[] s, int i, int j){
         char temp = s[i];
         s[i] = s[j];
