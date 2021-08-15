2d0c504840a2c28cf2695fd3db892a2ba0aa9152
Vijay Jammi <vjammi@gmail.com>
2021-08-15 02:04:03 -0400

    LC108 Convert Sorted Array to a BinarySearchTree

--- src/main/java/ds/patterns/trees/traversal/ConvertSortedArrayToBinarySearchTree.java
+++ src/main/java/ds/patterns/trees/traversal/ConvertSortedArrayToBinarySearchTree.java
@@ -5,4 +5,39 @@ package ds.patterns.trees.traversal;
 
  * */
 public class ConvertSortedArrayToBinarySearchTree {
+
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums.length ==0) return null;
+
+        return buildTree(nums, 0, nums.length-1);
+    }
+
+    public TreeNode buildTree(int[] nums, int start, int end) {
+
+        if (start>end){
+            return null;
+        }
+
+        int mid = (start+end)/2;
+        TreeNode node = new TreeNode(nums[mid]);
+
+        node.left  = buildTree(nums, start, mid-1);
+        node.right = buildTree(nums, mid+1, end);
+
+        return node;
+    }
 }
