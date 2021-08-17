052aaaa92987ee5e506e4debfb32aad45663b79f
Vijay Jammi <vjammi@gmail.com>
2021-08-16 22:31:08 -0400

    updates

--- src/main/java/ds/bst/ConvertSortedArrayToBinarySearchTree.java
+++ src/main/java/ds/bst/ConvertSortedArrayToBinarySearchTree.java
@@ -1,4 +1,4 @@
-package ds.patterns.trees.traversal;
+package ds.bst;
 
 /**
     108. Convert Sorted Array to Binary Search Tree   https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
