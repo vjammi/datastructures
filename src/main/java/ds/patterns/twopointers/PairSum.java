fe96c524789ca8a2ba069ea852704a3f5d6d7a63
Vijay Jammi <vjammi@gmail.com>
2021-08-21 22:52:56 -0400

    Pair Sum

--- src/main/java/ds/patterns/twopointers/PairSum.java
+++ src/main/java/ds/patterns/twopointers/PairSum.java
@@ -7,37 +7,43 @@ import java.util.Map;
 public class PairSum {
 
     /**
-     arr[] = {3, 5, 9, 2, 8, 10, 11} target = 17
-     arr[] = {2, 3, 5, 8, 9, 10, 11}
-     i =  ^
-     j =           ^
+                  0  1  2  3  4  5   6
+         arr[] = {3, 5, 9, 2, 8, 10, 11} target = 17
+         arr[] = {2, 3, 5, 8, 9, 10, 11}
+         i =                         ^
+         j =                     ^
      */
     private boolean isPairSumNaive1(int[] arr, int target) {
         Arrays.sort(arr);
-        for (int i = 0; i < arr.length+2-1; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                if (i != j) {  // because j==0 and we are looking for distinct pairs, we skip summing of itself, as a pair
+
+        int length = arr.length;
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                if (i != j) {               // We are looking for distinct pairs, not pair an element by itself. So we will skip if i == j
                     int sum = arr[i] + arr[j];
-                    if (sum == target) // pair found
+                    if (sum == target)      // pair found
                         return true;
-                    else if (sum > target)  // Since the array is sorted, we can break for this ith index and start the next ith index
+                    else if (sum > target)  // Since the array is sorted, we stop further iterating the jth index and start the next ith index
                         break;
-                   // else continue;        // Continue looking for the pair
+                    else
+                       continue;            // Continue looking for the pair
                 }
             }
         }
-        return false; // No pair found with given sum.
+        return false;                       // No pair found with given sum.
     }
 
     /**
+     *            0  1  2  3  4  5   6  7
          arr[] = {3, 5, 9, 2, 8, 10, 11} target = 17
          arr[] = {2, 3, 5, 8, 9, 10, 11}
-             i =  ^
-             j =           ^
+             i =                     ^
+             j =                        ^
     */
     private boolean isPairSumNaive2(int[] arr, int target) {
         Arrays.sort(arr);
-        for (int i = 0; i < arr.length+2-1; i++) {
+        for (int i = 0; i < arr.length; i++) {
+            // Note: When i=6, j=6+1=7, which is beyond the length of the array - so will never enter the inner for loop for j=7
             for (int j = i+1; j < arr.length; j++) { // Optimization: j=i+1
                 int sum = arr[i] + arr[j];
                 if (sum == target)      // pair found
@@ -77,7 +83,7 @@ public class PairSum {
         PairSum obj = new PairSum();
 
         int arr[] = {3, 5, 9, 2, 8, 10, 11}; // unsorted
-        int target = 17;
+        int target = 25; //17;
 
         System.out.println(obj.isPairSumNaive1(arr, target));
         System.out.println(obj.isPairSumNaive2(arr, target));
