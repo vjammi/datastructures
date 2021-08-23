b997437b5c2b1e51e647266ad4ad5e44d5245177
Vijay Jammi <vjammi@gmail.com>
2021-08-22 23:36:41 -0400

    LC Combination Sum 2

--- src/main/java/ds/patterns/backtracking/CombinationSumII.java
+++ src/main/java/ds/patterns/backtracking/CombinationSumII.java
@@ -7,9 +7,10 @@ import java.util.Stack;
 
 /**
      40	Combination Sum II  https://leetcode.com/problems/combination-sum-ii/
-     Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+     Given a collection of candidate numbers (candidates) and a target number (target),
+     find all unique combinations in candidates where the candidate numbers sum to target.
      Each number in candidates may only be used once in the combination.
-        Note: The solution set must not contain duplicate combinations.
+     Note: The solution set must not contain duplicate combinations.
 
      Example 1:
      Input: candidates = [10,1,2,7,6,1,5], target = 8
@@ -33,7 +34,7 @@ public class CombinationSumII {
         return result;
     }
 
-    public void combinationSum2(int[] candidates, int target, int sum, Stack<Integer> chosen, List<List<Integer>> result, int index){
+    public void combinationSum2(int[] input, int target, int sum, Stack<Integer> chosen, List<List<Integer>> result, int index){
 
         if (sum == target){
             result.add(new ArrayList(chosen) );
@@ -43,19 +44,19 @@ public class CombinationSumII {
             System.out.println("Sum>Target");
             return;
         }
-        else if (index == candidates.length){
+        else if (index == input.length){
             System.out.println("index==length");
             return;
         }
 
-        for (int i=index;i<candidates.length; i++){
-            System.out.println("Choice " +candidates[i] +" Sum " +sum);
-            if(i > index && candidates[i] == candidates[i-1]) {
-                System.out.println("Skipping " +candidates[i] +" Sum " +sum);
+        for (int i=index;i<input.length; i++){
+            System.out.println("Choice " +input[i] +" Sum " +sum);
+            if(i > index && input[i] == input[i-1]) {
+                System.out.println("Skipping " +input[i] +" Sum " +sum);
                 continue;
             }
-            chosen.push(Integer.valueOf(candidates[i]));
-            combinationSum2(candidates, target, sum+candidates[i], chosen, result, i+1);
+            chosen.push(Integer.valueOf(input[i]));
+            combinationSum2(input, target, sum+input[i], chosen, result, i+1);
             chosen.pop();
         }
     }
