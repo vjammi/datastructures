d483e61d913a71e8aa7e05f7427371bb6a859998
Vijay Jammi <vjammi@gmail.com>
2021-10-19 02:28:30 -0400

    Shortest Path Weighted graph - Dijkstra's / bfs

--- src/main/java/ds/graph/path/DijkstraShortestPathWeightedGraph.java
+++ src/main/java/ds/graph/path/DijkstraShortestPathWeightedGraph.java
@@ -6,28 +6,33 @@ import java.util.*;
 public class DijkstraShortestPathWeightedGraph {
     private Map<Integer, List<Pair<Integer,Integer>> > adjList;
 
-    public DijkstraShortestPathWeightedGraph(int noOfVertices) {
-        adjList =new HashMap<>();
-
-        for (int i=0; i<noOfVertices; i++){
-            adjList.put(i, new ArrayList<Pair<Integer,Integer>>());
-        }
-        // Pair(vertex, weight) - adjList represents vertex A(0) and vertex B(1) and the weight between A(0) & B(1) = 4
-        adjList.get(0).add(new Pair(1,4));
-        adjList.get(0).add(new Pair(2,1));
-        adjList.get(1).add(new Pair(4,4));
-        adjList.get(2).add(new Pair(1,2));
-        adjList.get(2).add(new Pair(3,4));
-        adjList.get(3).add(new Pair(4,4));
-    }
-
     class DistanceOfVertexFromSourceComparator implements Comparator<Pair<Integer,Integer>>{
         public int compare(Pair<Integer,Integer> a, Pair<Integer,Integer> b){
             return a.getValue().compareTo(b.getValue());
         }
     }
 
-      /**
+    public DijkstraShortestPathWeightedGraph() {
+    }
+
+    private void dijkstra(int noOfVertices) {
+        adjList =new HashMap<>();
+
+        for (int i=0; i<noOfVertices; i++){
+            adjList.put(i, new ArrayList<Pair<Integer,Integer>>());
+        }
+
+        // Pair(vertex, weight) - adjList represents vertex A(0) and vertex B(1) and the weight between A(0) & B(1) = 4
+        populateAdjList();
+
+        int[] path = new int[noOfVertices];
+        int[] distance = new int[noOfVertices];
+        Arrays.fill(distance,Integer.MAX_VALUE); // Initialize distance array ti infinity or -1
+
+        bfsDijkstra(0, noOfVertices - 1, path, distance);
+    }
+
+    /**
            Edge A - B distanceToNeighboringVertex: 4 priorDistance: 2147483647
            Edge A - C distanceToNeighboringVertex: 1 priorDistance: 2147483647
            Edge C - B distanceToNeighboringVertex: 3 priorDistance: 4
@@ -47,44 +52,54 @@ public class DijkstraShortestPathWeightedGraph {
                         C --4-- D
 
     */
-    private void dijkstra(int startVertex, int noOfVertices) {
-        int[] path = new int[noOfVertices];
-        int[] distance = new int[noOfVertices];
-        Arrays.fill(distance,Integer.MAX_VALUE);        // Initialize distance array ti infinity or -1
-
-        distance[startVertex] = 0;                      // Making distance for start vertex 0
-        path[startVertex] = startVertex;                // Updating path for start vertex to itself
+    private void bfsDijkstra(int sourceVertex, int noOfVertices, int[] path, int[] distance) {
+        // Store the reference of the previous vertex. Since this is the source we store the reference to itself.
+        distance[sourceVertex] = 0;
+        // Set the distance of the current vertex from the source vertex, in this case to itself, which will be 0
+        path[sourceVertex] = sourceVertex;
 
         //PriorityQueue<Pair<Integer,Integer>> priorityQueue = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
-        PriorityQueue<Pair<Integer,Integer>> priorityQueue = new PriorityQueue<>(new DistanceOfVertexFromSourceComparator());
-        priorityQueue.add(new Pair<>(startVertex,0));
+        PriorityQueue<Pair<Integer,Integer>> pq = new PriorityQueue<>(new DistanceOfVertexFromSourceComparator());
+        pq.add(new Pair<>(sourceVertex,0));
 
-        while(!priorityQueue.isEmpty()){
+        while(!pq.isEmpty()){
 
-            Pair<Integer,Integer> vertexObj = priorityQueue.remove();
-            Integer vertex = vertexObj.getKey();
+            Pair<Integer,Integer> currentVertexPair = pq.remove();
+            Integer currentVertex = currentVertexPair.getKey();
 
-            List<Pair<Integer,Integer>> neighbors = adjList.get(vertex);
+            List<Pair<Integer,Integer>> neighbors = adjList.get(currentVertex);
             for(Pair<Integer,Integer> neighborPair: neighbors){
-                int neighbor          = neighborPair.getKey();
-                int weightOfEdge      = neighborPair.getValue();
-                int distanceToNeighbor = distance[vertex] + weightOfEdge; // Distance of the source vertex plus the weight of the edge to the the visiting neighboring vertex
+                int neighbor           = neighborPair.getKey();
+                int weightOfEdge       = neighborPair.getValue();
+                int newCalculatedDistanceToNeighbor = distance[currentVertex] + weightOfEdge; // Distance of the source vertex plus the weight of the edge to the the visiting neighboring vertex
 
-                if(distanceToNeighbor < distance[neighbor]){
-                    if (distanceToNeighbor < distance[neighbor]) System.out.println("Edge " +(char)(vertex +'A') +" - " +(char)(neighbor +'A') +" distanceToNeighbor: " +distanceToNeighbor + " priorDistance: " +distance[neighbor]);
-                    distance[neighbor] = distanceToNeighbor;     // Distance of the source vertex plus the weight of the edge to the the visiting neighboring vertex
-                    path[neighbor] = vertex;                              // Will add the vertex from where we have come to this neighboring vertex
-                    priorityQueue.add(new Pair<>(neighbor, distance[neighbor]));
+                // If the newly calculated distance to neighbor [newCalculatedDistanceToNeighbor] < than the stored distance (distance[neighbor]) to neighbor [either infinity or previously calculated (visited) distance]
+                if(newCalculatedDistanceToNeighbor < distance[neighbor]){
+                    if (newCalculatedDistanceToNeighbor < distance[neighbor])
+                        System.out.println("Distance being updated - Edge " +(char)(currentVertex +'A') +" - " +(char)(neighbor +'A') +" distanceToNeighbor: " +newCalculatedDistanceToNeighbor + " priorDistance: " +distance[neighbor]);
+                    distance[neighbor] = newCalculatedDistanceToNeighbor; // Distance of the current vertex + the weight of the edge to the the this neighbor that is being visited
+                    path[neighbor] = currentVertex;  // We will add the reference of the currentVertex as the vertex from which we have come to this neighbor
+                    pq.add(new Pair<>(neighbor, distance[neighbor]));
+                }else {
+                    // if the newCalculatedDistanceToNeighbor > distance[neighbor], then we do not consider it
                 }
-
             }
         }
-        print(startVertex, noOfVertices, path, distance);
+        print(sourceVertex, noOfVertices, path, distance);
+    }
+
+    private void populateAdjList() {
+        adjList.get(0).add(new Pair(1,4));
+        adjList.get(0).add(new Pair(2,1));
+        adjList.get(1).add(new Pair(4,4));
+        adjList.get(2).add(new Pair(1,2));
+        adjList.get(2).add(new Pair(3,4));
+        adjList.get(3).add(new Pair(4,4));
     }
 
     public static void main(String[] args) {
-        DijkstraShortestPathWeightedGraph obj = new DijkstraShortestPathWeightedGraph(5);
-        obj.dijkstra(0,5);
+        DijkstraShortestPathWeightedGraph obj = new DijkstraShortestPathWeightedGraph();
+        obj.dijkstra(6);
     }
 
     private void print(int startVertex, int noOfVertices, int[] path, int[] distance) {
