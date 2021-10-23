08e37f6d22eb680b66bc7903a679f164618f7248
Vijay Jammi <vjammi@gmail.com>
2021-10-23 18:18:38 -0400

    Graph Traversal 101

--- src/main/java/ds/practice/graphs/Graph101.java
+++ src/main/java/ds/practice/graphs/Graph101.java
@@ -2,7 +2,7 @@ package ds.practice.graphs;
 
 import java.util.*;
 
-public class GraphTraversal {
+public class Graph101 {
     Map<Integer, List<Integer>> adjList;
 
     private void traverse(int vertices, int[][] edges) {
@@ -16,18 +16,23 @@ public class GraphTraversal {
             adjList.get(edge[0]).add(edge[1]);
             adjList.get(edge[1]).add(edge[0]);
         }
-        for (Map.Entry entry: adjList.entrySet()){
-            System.out.println("Key(vertex) " +entry.getKey() +" Value(neighbors) " +entry.getValue());
+        for (Map.Entry<Integer, List<Integer>> entry: adjList.entrySet()){
+            Integer key          = entry.getKey();
+            List<Integer>  value = entry.getValue();
+            System.out.println("Key(vertex) " + key +" Value(neighbors) " + value);
         }
 
-        ArrayList<Integer> chosen = new ArrayList<>();
-        int[] visited = new int[vertices];
+        System.out.println("--------------------dfs--------------------------");
+        ArrayList<Integer> chosen1 = new ArrayList<>();
+        int[] visited1 = new int[vertices];
+        dfs(0, adjList, visited1, chosen1); // Connected graph
+        System.out.println(chosen1);
 
-        //dfs(0, adjList, visited, chosen); // Connected graph
-        //System.out.println(chosen);
-
-        chosen.add(0);
-        dfsAllPaths(0, adjList, visited, chosen, new ArrayList());
+        System.out.println("------------------dfsAllPaths----------------------");
+        ArrayList<Integer> chosen2 = new ArrayList<>();
+        int[] visited2 = new int[vertices];
+        chosen2.add(0);
+        dfsAllPaths(0, adjList, visited2, chosen2, new ArrayList());
     }
 
     private void dfs(int vertex, Map<Integer, List<Integer>> adjList, int[] visited, List<Integer> path) {
@@ -72,7 +77,7 @@ public class GraphTraversal {
     public static void main(String[] args) {
         int vertices = 6;
         int[][] edges = {{0,1}, {0,2}, {1,3}, {2,3}, {2,4}, {4,5}, {3,5}};
-        GraphTraversal graph = new GraphTraversal();
+        Graph101 graph = new Graph101();
         graph.traverse(vertices, edges);
     }
 }
