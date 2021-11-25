dc563fb28c557ad4462bc7a23bae918b381378f2
Vijay Jammi <vjammi@gmail.com>
2021-11-24 23:44:15 -0500

    Sorted List to BST

--- src/main/java/ds/bst/SortedListToBST.java
+++ src/main/java/ds/bst/SortedListToBST.java
@@ -5,28 +5,6 @@ public class SortedListToBST {
     public class ListNode { int val; ListNode next; ListNode(int x) { val = x; } }
     public class TreeNode { int val; TreeNode left; TreeNode right; TreeNode(int x) { val = x; } }
 
-    private ListNode findMiddleElement(ListNode head) {
-
-        // The pointer used to disconnect the left half from the mid node.
-        ListNode prevPtr = null;
-        ListNode slowPtr = head;
-        ListNode fastPtr = head;
-
-        // Iterate until fastPr doesn't reach the end of the linked list.
-        while (fastPtr != null && fastPtr.next != null) {
-            prevPtr = slowPtr;
-            slowPtr = slowPtr.next;
-            fastPtr = fastPtr.next.next;
-        }
-
-        // Handling the case when slowPtr was equal to head.
-        if (prevPtr != null) {
-            prevPtr.next = null;
-        }
-
-        return slowPtr;
-    }
-
     public TreeNode sortedListToBST(ListNode head) {
 
         // If the head doesn't exist, then the linked list is empty
@@ -35,12 +13,14 @@ public class SortedListToBST {
         }
 
         // Find the middle element for the list.
-        ListNode mid = this.findMiddleElement(head);
+        ListNode mid = this.findMid(head);
 
         // The mid becomes the root of the BST.
         TreeNode node = new TreeNode(mid.val);
 
-        // Base case when there is just one element in the linked list
+        // Another base case - when there is just one element in the linked list
+        // When the node has no left and right siblings (only 1 element left) - head will be the mid.
+        // Then, we will no longer need to traverse its left and right subtree
         if (head == mid) {
             return node;
         }
@@ -49,5 +29,26 @@ public class SortedListToBST {
         node.left = this.sortedListToBST(head);
         node.right = this.sortedListToBST(mid.next);
         return node;
+    }
+
+    private ListNode findMid(ListNode head) {
+
+        ListNode prev = null; // The pointer used to disconnect the left half from the mid node.
+        ListNode slow = head; // Mid element
+        ListNode fast = head;
+
+        // Iterate until fastPr doesn't reach the end of the linked list.
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Disconnect mid/slow from its prev
+        if (prev != null) {
+            prev.next = null;
+        }
+
+        return slow;
     }
 }
