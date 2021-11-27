6afdf062f321ce05e6d92122a6431426dc45e07c
Vijay Jammi <vjammi@gmail.com>
2021-11-26 22:45:08 -0500

    LC 515 Find Largest Value in Each Tree Row

--- src/main/java/ds/patterns/trees/traversal/LargestValueInEachTreeRow.java
+++ src/main/java/ds/patterns/trees/traversal/LargestValueInEachTreeRow.java
@@ -6,7 +6,21 @@ import java.util.List;
 import java.util.Queue;
 
 /*
-Find the largest value in each Tree Row
+    515. Find Largest Value in Each Tree Row
+    Input: root = [1,3,2,5,3,null,9]
+    Output: [1,3,9]
+
+    Input: root = [1,2,3]
+    Output: [1,3]
+
+    Input: root = [1]
+    Output: [1]
+
+    Input: root = [1,null,2]
+    Output: [1,2]
+
+    Input: root = []
+    Output: []
 */
 public class LargestValueInEachTreeRow {
 
@@ -29,7 +43,37 @@ public class LargestValueInEachTreeRow {
         if (root == null)
             return new ArrayList<Integer>();
 
-        return bfs(root);
+        //return bfs(root);
+
+        List<Integer> result = new ArrayList<>();
+        dfs(root, result, 0);
+        return result;
+    }
+
+    /*
+        level       listSize
+        0           1
+        1           2
+        2           3
+    */
+    private void dfs(TreeNode node, List<Integer> result, int level){
+        if (node == null)
+            return;
+
+        if (result.size() <= level){                    // Level not created so far. Add new level to the list.
+            result.add(node.val);                       // Add the node val - first val
+        }else{                                          // Adding the already created level
+            Integer largestVal = Math.max(result.get(level), node.val);
+
+            // Can use either
+            // result.remove(level); result.add(level, largestVal);    // remove and add val at index
+            // or
+            result.set(level, largestVal);                             // Set val at index
+        }
+
+        dfs(node.left, result, level+1);
+        dfs(node.right, result, level+1);
+
     }
 
     private List<Integer> bfs(TreeNode root){
