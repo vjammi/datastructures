be4fa4412ab2742340faa9c55a40c778af1a474b
Vijay Jammi <vjammi@gmail.com>
2021-11-26 22:19:08 -0500

    Using a Generic Pair

---
+++ src/main/java/ds/usage/AGenericPair.java
@@ -0,0 +1,29 @@
+package ds.usage;
+
+public class AGenericPair {
+
+    class MyPair<K, V> {
+        private K key;
+        private V value;
+
+        public MyPair(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey(){ return key; }
+        public V getValue() { return value; }
+        public void setKey(K key)     { this.key = key; }
+        public void setValue(V value) { this.value = value; }
+    }
+
+    private void usage(){
+        MyPair<Integer, String> pair = new MyPair(Integer.valueOf(1), "One");
+        System.out.println(pair.key +" " + pair.value);
+    }
+
+    public static void main(String[] args) {
+        new AGenericPair().usage();
+    }
+
+}
