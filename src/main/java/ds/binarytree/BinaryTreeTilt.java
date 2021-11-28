940ed9e170389fdfc7e3264c40b892bfaee40fda
Vijay Jammi <vjammi@gmail.com>
2021-11-27 21:30:35 -0500

    LC 563 Binary Tree Tilt

--- src/main/java/ds/binarytree/BinaryTreeTilt.java
+++ src/main/java/ds/binarytree/BinaryTreeTilt.java
@@ -8,4 +8,48 @@ package ds.binarytree;
  The tilt of a tree node is the absolute difference between the sum of all left subtree node values and all right subtree node values. If a node does not have a left child, then the sum of the left subtree node values is treated as 0. The rule is similar if there the node does not have a right child.
 */
 public class BinaryTreeTilt {
+
+     public class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+         TreeNode() {}
+         TreeNode(int val) { this.val = val; }
+         TreeNode(int val, TreeNode left, TreeNode right) {
+             this.val = val;
+             this.left = left;
+             this.right = right;
+         }
+     }
+
+    // This problem requires you to
+    //   1. Calculate the Tilt of the two children [abs(left-right)]
+    //      1.1 Set the calculated tilt value as the new value of the node
+    //      1.2 Add the current tilt to the total running total for the tree [totalTilt = totalTilt + tilt]
+    //   2. Calculate the sum of all the values of the subtree, and return to its parent [subTreeSum = node.val + left + right]
+    public int dfs(TreeNode node) {
+        if (node == null)
+            return 0;
+
+        int left = dfs(node.left);
+        int right = dfs(node.right);
+
+        int tilt = Math.abs(left - right);
+        totalTilt = totalTilt + tilt;
+
+        int subTreeSum = node.val + left + right;
+        node.val = tilt;
+
+        return subTreeSum;
+    }
+    int totalTilt = 0;
+
+    public int findTilt(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        dfs(root);
+
+        return totalTilt;
+    }
 }
