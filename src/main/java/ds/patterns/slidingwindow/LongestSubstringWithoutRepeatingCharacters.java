a68e3a6477ef99409cde7b60f0cd05ac52c26c8b
Vijay Jammi <vjammi@gmail.com>
2021-11-30 23:28:08 -0500

    LC3 Longest Substring Without Repeating Characters

--- src/main/java/ds/patterns/slidingwindow/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/ds/patterns/slidingwindow/LongestSubstringWithoutRepeatingCharacters.java
@@ -31,7 +31,7 @@ public class LongestSubstringWithoutRepeatingCharacters {
      s=abcbbcbb
      longest=3
      */
-    public int lengthOfLongestSubstring(String s) {
+    public int lengthOfLongestSubstring1(String s) {
         if (s == null || s.length() == 0)
             return 0;
 
@@ -76,6 +76,50 @@ public class LongestSubstringWithoutRepeatingCharacters {
             }
             System.out.println(j +" - " +i +" - "+longest);
         }
+        return longest;
+    }
+
+    // Input: s = "abcabcbb"
+    //            "tmmzuxt"
+    //        i          ^
+    //        j      ^
+    // Output: 3
+    public int lengthOfLongestSubstring(String s) {
+
+        int longest = 0;
+        int longestSubStrLength = 0;
+        int j=0;
+        Set<Character> set = new HashSet<>();
+        for (int i=0; i< s.length(); i++){
+
+            Character c = s.charAt(i);
+
+            if (!set.contains(c)){
+                set.add(c);
+                longestSubStrLength++; // 3
+                longest = Math.max(longest, longestSubStrLength);
+            }else{
+                // Increment j until it reaches the duplicate char
+                // In the meantime you will remove the chars you will encounter.
+                while(s.charAt(j) != c) {
+                    set.remove(s.charAt(j));
+                    j++;
+                    longestSubStrLength--;
+                }
+
+                // Once j is at char c, we remove from the set, increment j and decrement length
+                set.remove(s.charAt(j));
+                j++;
+                longestSubStrLength--;
+
+                // Now add new char to the set, increment longestSubStrLength and get max length
+                set.add(c);
+                longestSubStrLength++;
+                longest = Math.max(longest, longestSubStrLength);
+            }
+
+        }
+
         return longest;
     }
 
