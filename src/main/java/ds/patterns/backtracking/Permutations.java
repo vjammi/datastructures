8feb089191660c69b754d044a8f248045e4d82bd
Vijay Jammi <vjammi@gmail.com>
2021-12-06 21:11:58 -0500

    LC46 Permutations

--- src/main/java/ds/patterns/backtracking/Permutations.java
+++ src/main/java/ds/patterns/backtracking/Permutations.java
@@ -13,65 +13,114 @@ import java.util.List;
     Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
  */
 public class Permutations {
-    //              INPUT|CHOICE
-    //                ABC|
 
-    //               0-BC|A
-    //                0-C|AB
-    //                  _|ABC
-    //                0-B|AC
-    //                  _|ACB
+    /**
+     //              INPUT|CHOICE
+     //                ABC|
 
-    //               1-AC|B
+     //               0-BC|A
+     //                0-C|AB
+     //                  _|ABC
+     //                0-B|AC
+     //                  _|ACB
 
-    //                                       _|ABC
-    //                               0-C|AB                // C inserted back at the 0th index - C
-    //                                 0
-    //                      0-BC|A                         // B inserted back 0th index resulting in BC, C inserted back at 1st index resulting in BC
-    //                        01
-    //                               1-B|AC                // B is put back at the 0th index
-    //                                 0
-    //                                       _|ACB
+     //               1-AC|B
 
-    //                                       _|BAC
-    //                               0-C|BA                // On the way back B is put back at the 0th index
-    //                                 0
-    //             ABC|     1-AC|B
-    //             012        01     1-A|BC
-    //                                 0
-    //                                       _|BCA
+     //                                         0-4x
+     //
+     //                                                 4x
+     //                                0-4      1-5     5x
+     //                                                 6   [456]
 
-    //                                       _|CAB
-    //                               0-B|CA
-    //                                 0
-    //                      2-AB|C
-    //                        01
-    //                               1-A|CB
-    //                                 0
-    //                                       _|CBA
+     //                                                 4x
+     //                                         2-6     5   [465]
+     //                                                 6x
+     //
+     //                                         0-4
+     //                         456    1-5      1-5
+     //                                         2-6
+     //
+     //                                         0-4
+     //                                2-6      1-5
+     //                                         2-6
 
+
+
+     //                                       _|ABC
+     //                               0-C|AB                // C inserted back at the 0th index - C
+     //                                 0
+     //                      0-BC|A                         // B inserted back 0th index resulting in BC, C inserted back at 1st index resulting in BC
+     //                        01
+     //                               1-B|AC                // B is put back at the 0th index
+     //                                 0
+     //                                       _|ACB
+
+     //                                       _|BAC
+     //                               0-C|BA                // On the way back B is put back at the 0th index
+     //                                 0
+     //             ABC|     1-AC|B
+     //             012        01     1-A|BC
+     //                                 0
+     //                                       _|BCA
+
+     //                                       _|CAB
+     //                               0-B|CA
+     //                                 0
+     //                      2-AB|C
+     //                        01
+     //                               1-A|CB
+     //                                 0
+     //                                       _|CBA
+     */
+
+
+    // Solution using N connected components
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList();
-        if (nums.length == 0)
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+        if (nums.length ==  0)
             return result;
 
-        List<Integer> input = new ArrayList();
-        for (int num: nums){
-            input.add(num);
-        }
+        boolean[] visited = new boolean[nums.length];
+        List<Integer> path = new ArrayList<>();
+        dfs1(nums, path, result, visited);
 
-        List<Integer> chosen = new ArrayList();
-        //backtrack(input, 0, chosen, result);
-        backtrack(nums, 0, chosen, result);
+        // List<Integer> chosen = new ArrayList();
+        // backtrack1(nums, 0, chosen, result);
+
+        // List<Integer> input = new ArrayList();
+        // List<Integer> chosen = new ArrayList();
+        // for (int num: nums)
+        //     input.add(num);
+        // backtrack2(input, chosen, result);
 
         return result;
     }
 
-    // Permuting 4 elements is - Picking/Choosing 1 element and permuting the other 3
-    private void backtrack(int[] nums, int index, List<Integer> chosen, List<List<Integer>> result){
+
+    public void dfs1(int[] nums, List<Integer> path, List<List<Integer>> result, boolean[] visited){
+        if (path.size() == nums.length){
+            result.add(new ArrayList(path));
+            return;
+        }
+
+        for (int i=0;i<nums.length; i++){
+            if(!visited[i]){
+                path.add(nums[i]);
+                visited[i] = true;
+
+                dfs1(nums, path, result, visited);
+
+                path.remove(path.size()-1);
+                visited[i] = false;
+            }
+
+        }
+    }
+
+    private void backtrack1(int[] nums, int index, List<Integer> chosen, List<List<Integer>> result){
         if (chosen.size() == nums.length){
-            List<Integer> list = new ArrayList(chosen);
-            result.add(list); System.out.println(list);
+            result.add(new ArrayList(chosen)); System.out.println(new ArrayList(chosen));
             return;
         }
 
@@ -79,24 +128,22 @@ public class Permutations {
             int choice = nums[i];
 
             if(chosen.contains(choice)) {
-                System.out.println("            "+i +"***" + index+"***"+choice);
+                //System.out.println(i +"***" + index+"***"+choice);
                 continue;
             }
-            System.out.println(i +" " +index+" "+choice);
 
+            //System.out.println(i +" " +index+" "+choice);
             chosen.add(choice);
-            backtrack(nums, i+1, chosen, result);
+            backtrack1(nums, i, chosen, result);
             chosen.remove(chosen.size()-1);
         }
 
     }
 
     // Permuting 4 elements is - Picking/Choosing 1 element and permuting the other 3
-    private void backtrack2(List<Integer> input, int index, List<Integer> chosen, List<List<Integer>> result){
+    private void backtrack2(List<Integer> input, List<Integer> chosen, List<List<Integer>> result){
         if (input.size() == 0){
-            List<Integer> list = new ArrayList(chosen);
-            result.add(list);
-            System.out.println(list);
+            result.add(new ArrayList(chosen)); System.out.println(new ArrayList(chosen));
             return;
         }
 
@@ -106,13 +153,14 @@ public class Permutations {
             chosen.add(choice);
             input.remove(i);
 
-            backtrack2(input, i+1, chosen, result);
+            backtrack2(input, chosen, result);
 
             chosen.remove(chosen.size()-1);
             input.add(i, choice);
         }
 
     }
+
 
     public static void main(String[] args) {
         new Permutations().permute(new int[]{4, 5, 6});
