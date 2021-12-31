56a276c8ce811e8feccaa890f9dd931f13c1a3ac
Vijay Jammi <vjammi@gmail.com>
2021-12-30 23:52:55 -0500

    LC207 Course Schedule

--- src/main/java/ds/patterns/graph/CourseSchedule.java
+++ src/main/java/ds/patterns/graph/CourseSchedule.java
@@ -5,27 +5,52 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/*
-    207. Course Schedule
-    There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1.
-    You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+    /**
+        207. Course Schedule
+        There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1.
+        You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
 
-    For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
-    Return true if you can finish all courses. Otherwise, return false.
+        For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+        Return true if you can finish all courses. Otherwise, return false.
 
-    Example 1:
-    Input: numCourses = 2, prerequisites = [[1,0]]
-    Output: true
-    Explanation: There are a total of 2 courses to take.
-    To take course 1 you should have finished course 0. So it is possible.
+        Example 1:
+        Input: numCourses = 2, prerequisites = [[1,0]]
+        Output: true
+        Explanation: There are a total of 2 courses to take.
+        To take course 1 you should have finished course 0. So it is possible.
 
-    Example 2:
-    Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
-    Output: false
-    Explanation: There are a total of 2 courses to take.
+        Example 2:
+        Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+        Output: false
+        Explanation: There are a total of 2 courses to take.
 
-    To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
-*/
+        To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
+    */
+
+//    /**
+//         Graph needs to be directed, connected??? with NO Cycles
+//         i.e. [2->1] [1->0]
+//         Here,
+//         numCourses: Nodes of the graph
+//         prerequisites: Edges between the nodes of the graph
+//
+//         numCourses = 9
+//         prerequisites: [[8,7],[7,6],[6,5],[5,4],[4,3],[3,2],[2,1],[1,0]]
+//         adjList: {  0:[],   1:[0],  2:[1],  3:[2],  4:[3],  5:[4],  6:[5],  7:[6],  8:[7]   }
+//         canFinish: true
+//           7       5       3      1
+//          /  \   /   \   /   \  /  \
+//         8     6        4      2     0
+//
+//         numCourses = 9
+//         prerequisites: [[8,7], [7,6], [6,5], [5,4], [4,3], [3,2], [2,1], [2,4], [1,0]]
+//         adjList: {  0:[],   1:[0],  2:[1,4],  3:[2],  4:[3],  5:[4],  6:[5],  7:[6],  8:[7]   }
+//         canFinish: false
+//
+//            7       5       3      1
+//          /  \   /   \   /   \  /  \
+//         8     6        4   -  2     0
+//    */
 
 public class CourseSchedule {
     // int[][] prerequisites = {{0,1},{1,2}, {3,2}, {4,3}, {2,4}};
