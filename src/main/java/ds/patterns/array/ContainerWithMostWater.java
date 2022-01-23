6a6a09ef78f369fd5a41ec8e6fe45b382ec2bcd3
Vijay Jammi <vjammi@gmail.com>
2022-01-22 21:12:12 -0500

    LC11 Container With Most Water

--- src/main/java/ds/patterns/array/ContainerWithMostWater.java
+++ src/main/java/ds/patterns/array/ContainerWithMostWater.java
@@ -1,6 +1,6 @@
 package ds.patterns.array;
 
-/*
+/**
     11. Container With Most Water
     You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
     Find two lines that together with the x-axis form a container, such that the container contains the most water.
@@ -11,7 +11,7 @@ package ds.patterns.array;
     Output: 49
     Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
 
-**/
+ */
 public class ContainerWithMostWater {
 
     // Runtime: O(n^2)
