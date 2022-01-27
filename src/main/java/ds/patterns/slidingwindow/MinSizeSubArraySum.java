6f5b4a6d89480092c1b9bbaad54367576a63c230
Vijay Jammi <vjammi@gmail.com>
2022-01-27 18:54:47 -0500

    LC209 Minimum Size Subarray Sum

--- src/main/java/ds/patterns/slidingwindow/MinSizeSubArraySum.java
+++ src/main/java/ds/patterns/slidingwindow/MinSizeSubArraySum.java
@@ -1,26 +1,28 @@
 package ds.patterns.slidingwindow;
 
-// LC-209
-// Window size changes overtime
-/**
- 209. Minimum Size Subarray Sum
- Given an array of positive integers nums and a positive integer target, return the minimal length of a contiguous
- subarray [numsl, numsl+1, ..., numsr-1, numsr] of which the sum is greater than or equal to target. If there is no
- such subarray, return 0 instead.
+    /**
+     209. Minimum Size Subarray Sum
+         Given an array of positive integers nums and a positive integer target, return the minimal length of a contiguous
+         subarray [numsl, numsl+1, ..., numsr-1, numsr] of which the sum is greater than or equal to target. If there is no
+         such subarray, return 0 instead.
 
- Example 1:             8
- Input: nums = [2 3 1 2 4 3]
- ^     ^
- i   j
- target = 7
- Output: 2
- Explanation: The subarray [4,3] has the minimal length under the problem constraint.
- */
-//Naive solution would be to find a nexted forloop - o(N2)
-// Naive solution would be to use a nested for loop - o(N2)
-// Setup 2 pointers - start and end pointers.
-// Move the end pointer until a window is found. Then adjust the window by moving the start pointer.
-// then we move the end pointer to find next window
+         Input: nums = [2 3 1 2 4 3]
+                        ^     ^
+                        j     i
+                            sum=8
+         target = 7
+         Output: 2
+         Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+
+    Solution
+        Brute Force
+             Naive solution would be to use a nested for loop - o(N2)
+
+        2 Pointers - Window size changes overtime
+            Setup 2 pointers - start and end pointers.
+            Move the start pointer until a window is found. Then adjust the window by moving the end pointer.
+            then we move the start pointer to find next window
+    */
 
 public class MinSizeSubArraySum {
 
@@ -28,18 +30,23 @@ public class MinSizeSubArraySum {
         if (nums == null || nums.length == 0)
             return  0;
 
-        int i=0; int sum=0; int minLength = Integer.MAX_VALUE;
-        for (int j=0;j<nums.length;j++){
-            sum = sum + nums[j];
+        int minLength = Integer.MAX_VALUE;
+        int sum=0;
+        int end=0;
+        for (int start=0;start<nums.length;start++){
+
+            sum = sum + nums[start];
             if (sum >= target)
-                minLength = Math.min(minLength, (j-i) + 1);
+                minLength = Math.min(minLength, (start-end) + 1);
+
             while (sum >= target){
-                minLength = Math.min(minLength, (j-i) + 1);
-                System.out.println("Sum: " + sum +" minLength: " +minLength+ " i: " + i + " j: " + j);
-                sum = sum - nums[i];
-                i=i+1;
+                minLength = Math.min(minLength, (start-end) + 1);
+                System.out.println("Sum: " + sum +" minLength: " +minLength+ " end: " + end + " start: " + start);
+                sum = sum - nums[end];
+                end = end+1;
             }
         }
+
         return (minLength != Integer.MAX_VALUE) ? minLength : 0;
     }
 
