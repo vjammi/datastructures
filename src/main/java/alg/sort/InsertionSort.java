fe114d3fc571425d2a8a1966c2bb1c99b833567c
Vijay Jammi <vjammi@gmail.com>
2022-04-12 00:16:06 -0400

    InsertionSort updates...

--- src/main/java/alg/sort/InsertionSort.java
+++ src/main/java/alg/sort/InsertionSort.java
@@ -30,6 +30,7 @@ public class InsertionSort {
         }
         System.out.println();
     }
+
     public static void main(String[] args){
         InsertionSort obj = new InsertionSort();
         int[] arr = {15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
