76b956c4f0542a255f0182511bc3fdb48c47a7c6
Vijay Jammi <vjammi@gmail.com>
2022-05-09 17:08:54 -0400

    .

--- src/main/java/ds/patterns/trees/path/PathSum.java
+++ src/main/java/ds/patterns/trees/path/PathSum.java
@@ -1,6 +1,19 @@
 package ds.patterns.trees.path;
 
-// 115 out of 116 Tests Cases pass
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 112. Path Sum
+ * Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+ * A leaf is a node with no children.
+ *
+ * Example 1:
+ * Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+ * Output: true
+ * Explanation: The root-to-leaf path with the target sum is shown.
+ * */
 public class PathSum {
 
     public class TreeNode {
@@ -17,29 +30,55 @@ public class PathSum {
     }
 
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        if (root == null)
-            return false;
+        return dfs(root, targetSum);
 
-        if (targetSum == 0 && root !=null)
-            return false;
-
-        return pathSum(root, targetSum);
+        //    Stack<Integer> stack = new Stack<Integer>();
+        //    List<List<Integer>> result = new ArrayList<>();
+        //    boolean hasPath = dfsDebug(root, targetSum, stack, result);
+        //    System.out.println("Final Result " +result);
+        //    return hasPath;
     }
 
-    boolean pathSum(TreeNode node, int sum){
-        if (node == null)   // We have already checked if sum == 0 before it gets null and have returned.
+    public boolean dfs(TreeNode node, int targetSum) {
+        if (node == null)
             return false;
 
-        sum = sum - node.val;
-        if (node.left == null && node.right == null && sum == 0)
+        if (node.left == null && node.right == null && (targetSum - node.val) == 0 )
             return true;
 
-        boolean left  = pathSum(node.left, sum);
-        boolean right = pathSum(node.right, sum);
+        boolean leftTrue = dfs(node.left,  targetSum-node.val);
+        boolean rightTrue = dfs(node.right, targetSum-node.val);
 
-        if(left || right)
+        if (leftTrue || rightTrue)
             return true;
-        else
-            return false;
+
+        return false;
     }
+
+
+    public boolean dfsDebug(TreeNode node, int targetSum, Stack<Integer> chosen, List<List<Integer>> result) {
+        if (node == null){
+            return false;
+        }
+
+        chosen.push(node.val);
+
+        if (node.left == null && node.right == null && (targetSum - node.val) == 0 ){
+            result.add(new ArrayList(chosen));
+            System.out.println("Chosen " +chosen);
+            System.out.println("Result " +result);
+            return true;
+        }
+
+        boolean leftTrue = dfsDebug(node.left,  targetSum-node.val, chosen, result);
+        boolean rightTrue = dfsDebug(node.right, targetSum-node.val, chosen, result);
+
+        chosen.pop();
+
+        if (leftTrue || rightTrue)
+            return true;
+
+        return false;
+    }
+
 }
